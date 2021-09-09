77c144f37fb71b4790bd3958180d4c3bbe0a54d9
Ramom Karllos <rkzeraa@gmail.com>
2021-09-09 00:15:09 -0300

    Inicialização Projeto Bancario

---
+++ AgenciaBancaria.App/Program.cs
@@ -0,0 +1,23 @@
+﻿using AgenciaBancaria.Dominio;
+using System;
+
+namespace AgenciaBancaria.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+
+                Cliente cliente = new Cliente("Ramom", "123123", "321321",
+                    "Minha Rua", "12345678", "Recife", "PE");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
